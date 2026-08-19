465e6aab8b0f4fb8bd9d9db449ac839965d63b74
thomaspires-data <thomas.correia@buser.com.br>
2026-08-19 12:44:57 -0300

    Teste Few Shot prompt de empresas tipo vendas

--- formatos/descricao/interacao_ia_descricao.py
+++ formatos/descricao/interacao_ia_descricao.py
@@ -186,5 +186,5 @@ def gerar_descricao_por_tema(
 
 
 if __name__ == "__main__":
-    texto = gerar_descricao_por_tema("ALLESTUR", tom="vendas", media_palavras=100)
+    texto = gerar_descricao_por_tema("Expresso JK", tom="vendas", media_palavras=100)
     print(texto)
